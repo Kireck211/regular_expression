20511e6c28031330c064c9709aab6355a2b27e20
Kireck211 <erickdesantiago11@gmail.com>
2016-11-21 23:40:24 -0600

    Checking ñ transitions

--- src/automata.py
+++ src/automata.py
@@ -17,7 +17,7 @@ def main():
 	input_file = codecs.open(file_name, encoding='utf-8')
 	regular_expression = input_file.readline()
 	module1 = m1(regular_expression)
-	print("Module 1 (RPN): " +module1, end="\n\n")
+	print("Module 1 (RPN): " + module1, end="\n\n")
 	print("Module 2 (AFN-e):")
 	module2 = m2(module1)
 
